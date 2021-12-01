ee48c6b9fc388f14e95d6494eb32ac2d7e872a7f
dzaky1201 <dzakyhdr@gmail.com>
2021-12-01 11:13:29 +0700

    crud laravel

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    public function index()
+    {
+        $mahasiswas = Mahasiswa::all();
+        return view('mahasiswa.index', ['mahasiswas' => $mahasiswas]);
+    }
+    public function create()
+    {
+        return view('mahasiswa.create');
+    }
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'nim' => 'required|size:8|unique:mahasiswas',
+            'nama' => 'required|min:3|max:50',
+            'jenis_kelamin' => 'required|in:P,L',
+            'jurusan' => 'required',
+            'alamat' => '',
+        ]);
+
+        // create data menggunakan mass assigment
+        Mahasiswa::create($validateData);
+
+        // flash data (ketika data berhasil diinput maka akan ada alert dialaog dan redirect ke index)
+        return redirect()->route('mahasiswas.index')->with(
+            'pesan',
+            "Penambahan data {$validateData['nama']} berhasil"
+        );
+    }
+
+    public function show(Mahasiswa $mahasiswa)
+    {
+        // route model binding, ini cara magic untuk menampilkan page selanjutnya sesuai dengan id
+        return view('mahasiswa.show', ['mahasiswa' => $mahasiswa]);
+    }
+
+    public function edit(Mahasiswa $mahasiswa)
+    {
+        return view('mahasiswa.edit', ['mahasiswa' => $mahasiswa]);
+    }
+
+    public function update(Request $request, Mahasiswa $mahasiswa)
+    {
+        $validateData = $request->validate([
+            'nim' => 'required|size:8|unique:mahasiswas,nim,' . $mahasiswa->id,
+            'nama' => 'required|min:3|max:50',
+            'jenis_kelamin' => 'required|in:P,L',
+            'jurusan' => 'required',
+            'alamat' => '',
+        ]);
+        Mahasiswa::where('id', $mahasiswa->id)->update($validateData);
+        return redirect()->route('mahasiswas.show', ['mahasiswa' => $mahasiswa->id])
+            ->with('pesan', "Update data {$validateData['nama']} berhasil");
+    }
+
+    public function destroy(Mahasiswa $mahasiswa)
+    {
+        $mahasiswa->delete();
+        return redirect()->route('mahasiswas.index')
+            ->with('pesan', "Hapus data $mahasiswa->nama berhasil");
+    }
+}
